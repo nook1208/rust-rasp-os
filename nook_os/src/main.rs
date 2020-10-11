ef02c1796f38e7113e85bd0a1b1e215b0b22f390
Sunwook Eom <sunwook5492@gmail.com>
2020-10-11 19:00:40 +0900

    "Tutorail 02 - Runtime init" Done !
    Signed-off-by: Sunwook Eom <sunwook5492@gmail.com>

--- nook_os/src/main.rs
+++ nook_os/src/main.rs
@@ -7,4 +7,15 @@
 
 mod bsp;
 mod cpu;
+mod memory;
 mod panic_wait;
+mod runtime_init;
+
+/// Early init code.
+///
+/// # Safety
+///
+/// - Only a single core must be active and running this function.
+unsafe fn kernel_init() -> ! {
+    panic!()
+}
